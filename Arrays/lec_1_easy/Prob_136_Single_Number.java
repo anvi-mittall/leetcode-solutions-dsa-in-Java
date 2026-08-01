131af62b33520f74713451c242da45b4316dc774
Anvi Mittal <b241130@skit.ac.in>
2026-08-01 20:45:41 +0530

    SingleNumber

---
+++ Arrays/lec_1_easy/Prob_136_Single_Number.java
@@ -0,0 +1,27 @@
+package Arrays.lec_1_easy;
+
+public class Prob_136_Single_Number {
+    public int singleNumber(int[] nums){
+        int n = nums.length;
+
+        for(int i=0; i<n; i++){
+            int count = 0;
+
+            for(int j=0; j<n; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+            if(count == 1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]){
+        Prob_136_Single_Number s = new Prob_136_Single_Number();
+        int[] nums = {1,2,2,4,3,1,4};
+        System.out.println(s.singleNumber(nums));
+    }
+}
