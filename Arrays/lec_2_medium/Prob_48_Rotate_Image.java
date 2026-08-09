becebebd2345b0b8eb066eabfaebe904f0f52327
Anvi Mittal <b241130@skit.ac.in>
2026-08-09 19:18:47 +0530

    rotateImage

---
+++ Arrays/lec_2_medium/Prob_48_Rotate_Image.java
@@ -0,0 +1,40 @@
+package Arrays.lec_2_medium;
+
+public class Prob_48_Rotate_Image {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            int start = 0;
+            int end = n-1;
+            while(start < end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        Prob_48_Rotate_Image s = new Prob_48_Rotate_Image();
+        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+        s.rotate(matrix);
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
