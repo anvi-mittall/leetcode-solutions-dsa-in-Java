3bcfccbb4099262255e16ae00ed06635728aed20
Anvi Mittal <b241130@skit.ac.in>
2026-08-10 20:11:03 +0530

    MajorityElementII

---
+++ Arrays/lec_2_medium/Prob_229_Majority_Element_II.java
@@ -0,0 +1,36 @@
+package Arrays.lec_2_medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prob_229_Majority_Element_II {
+    public List<Integer> majorityElementTwo(int[] nums){
+        List<Integer> ans  = new ArrayList<>();
+
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(ans.contains(nums[i])){
+                continue;
+            }
+
+            int count = 0;
+            for(int j=0; j<n; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+
+            if(count > n/3){
+                ans.add(nums[i]);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String args[]){
+        Prob_229_Majority_Element_II s = new Prob_229_Majority_Element_II();
+        int[] nums = {1,2,1,1,3,2};
+        List<Integer> result = s.majorityElementTwo(nums);
+        System.out.println(result);
+    }
+}
