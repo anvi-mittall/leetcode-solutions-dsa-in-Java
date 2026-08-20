f9f70a2e0c08508245574ca9a7b7a1f41cd23900
Anvi Mittal <b241130@skit.ac.in>
2026-08-20 20:00:21 +0530

    searchInRotatedSortedArray

---
+++ Binary_Search/BS_on_1D_Arrays/Prob_33_Search_in_rotated_sorted_array.java
@@ -0,0 +1,38 @@
+package Binary_Search.BS_on_1D_Arrays;
+
+public class Prob_33_Search_in_rotated_sorted_array {
+    public int search(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target < nums[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+
+            else{
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]){
+        Prob_33_Search_in_rotated_sorted_array s = new Prob_33_Search_in_rotated_sorted_array();
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(s.search(nums, target));
+    }
+}
